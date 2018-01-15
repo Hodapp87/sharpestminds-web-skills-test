79f8b7bea6a48c8f076479ad9d5228d7b1745427
Chris Hodapp <hodapp87@gmail.com>
2018-01-15 12:17:42 -0500

    Added code for creating playlist

--- scrape.py
+++ scrape.py
@@ -29,20 +29,12 @@ class SetListHTTPException(Exception):
 class SetListNotFoundException(Exception):
     pass
     
-# Request headers sent with every API call:
-headers = {
-    # API key is hard-coded (bad practice, but it's allowed in the
-    # other Python module too):
-    "x-api-key": "7445bfb6-4ab8-4900-9929-59bfaec097f0",
-    # We only want JSON results:
-    "Accept": "application/json",
-}
-
 def json_pprint(j):
     """Utility-function to pretty-print a parsed JSON tree."""
     print(json.dumps(j, indent=4, sort_keys=True))
 
-def find_songs(artist, base_url="https://api.setlist.fm/rest/1.0"):
+def find_songs(artist, base_url="https://api.setlist.fm/rest/1.0",
+               api_key = "7445bfb6-4ab8-4900-9929-59bfaec097f0"):
     """Obtains the songs from a musical artist's latest set-list.  This
     scrapes http://www.setlist.fm in order to get this data.
 
@@ -50,17 +42,18 @@ def find_songs(artist, base_url="https://api.setlist.fm/rest/1.0"):
     artist -- String with the musician or band's name
     base_url -- Optional string with URL prefix for setlist.fm API
                 (default: https://api.setlist.fm/rest/1.0)
+    api_key -- Optional API key for api.setlist.fm
 
     Returns:
     songs -- A list of song names (as strings)
     """
-    # api.setlist.fm's API will return setlists for an artist, but the
-    # artist must be given by its MusicBrainz ID.  Thus, we must first
-    # query for artists, and receive most-relevant results first:
-    params = {
-        "artistName": artist,
-        "sort": "relevance"
-    }
+    # Sent with every request to force JSON and to pass API key:
+    headers = { "x-api-key": api_key, "Accept": "application/json" }
+    # I can use the API's /1.0/search/setlists to search directly by
+    # artist, but I have run into cases where this returns the wrong
+    # artist first.  Searching the artist first, sorted by relevance,
+    # then searching for setlists by MBID seems to work better.
+    params = { "artistName": artist, "sort": "relevance" }
     artist_url = base_url + "/search/artists"
     resp = requests.get(artist_url, params, headers=headers)
     if resp.status_code != requests.codes.ok:
--- spotify.py
+++ spotify.py
@@ -9,17 +9,16 @@
 import scrape
 import spotipy
 import spotipy.util
+import datetime
+
+SPOTIFY_USERNAME = "hodapp"
 
 token = spotipy.util.prompt_for_user_token(
-    "hodapp",
-    "playlist-modify-private",
+    SPOTIFY_USERNAME,
+    "playlist-modify-public",
     client_id = "caaa12900a34490dbba42487f4f37923",
     client_secret = "ada72b869ccf4cf4afb16560d95c956c",
-    redirect_uri = "http://localhost:8888/callback?code=AQC5dPffRz09FPxhjLqhqBjejo4jIqUlH9VmZanq-qSkqGVhFYroZ0FbuUxTUOVnpDdqHIxyCqvd3MYPk9EbJJ38dyH1z5r120pvPWZE6cESdDsOxxbqnPD1vFt9Bd4tawqhY9avBLotbn-UfIUJ2Tixr8CuCmBME4UGgoQngHvkrdRqJhGcHUkK-EBt_wPi2b2NUp3BDeHbqBdrk6LZ-RNz-ezBmztsGxlFZSg")
-
-# sp = spotipy.Spotify(auth=token)
-# sp.user_playlist_create
-# sp.user_playlist_add_tracks
+    redirect_uri = "http://localhost:8888/callback?code=AQCm02r9a0N_7CV02FmQRnTKH-5ICNg9lZBsbBuEIuPu03Tg4Neu0U97HrtzJNkJzQIGY-_k4wf3OmpuSf1KbouSKr0L_bhTiZawIRUTDr-wLuKaPcFRvZTOW_LWy69mddIi2XM3mNjLp-39kJSm6i1fZXOFj2U908r5IfmKOelhb8Zp0xRFe6qyeThPcub-7RXnV7qSVQkOU4sZ1inLwqgoKP4VfchZS_Y_Kg")
 
 def generate_playlist(songs):
     """Given a list of songs, creates a Spotify playlist which contains
@@ -31,4 +30,27 @@ def generate_playlist(songs):
     Returns:
     playlist_link -- A link to a Spotify playlist
     """
-    pass
+    sp = spotipy.Spotify(auth=token)
+    
+    # Query for Spotify's song IDs:
+    song_ids = []
+    for song in songs:
+        r = sp.search('track:"{}"'.format(song), type="track")
+        tracks = r["tracks"]["items"]
+        if tracks:
+            song_ids.append(tracks[0]["id"])
+        else:
+            print('Skipping song "{}" because no match found'.format(song))
+
+    # Create a new playlist and get its URL and ID:
+    dt = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    playlist_name = "SharpestMinds test " + dt
+    r = sp.user_playlist_create(SPOTIFY_USERNAME, playlist_name)
+    print('Created playlist \"{}\"'.format(playlist_name))
+    playlist_id = r["id"]
+    playlist_url = r["external_urls"]["spotify"]
+    
+    # Add songs to the playlist:
+    sp.user_playlist_add_tracks(SPOTIFY_USERNAME, playlist_id, song_ids)
+    
+    return playlist_url
